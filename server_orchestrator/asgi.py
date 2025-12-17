cbb8c4b8df6f32445c55f3f6b1df5ed484cf5b64
Al-amin Khalid <alaminkhalid2@gmail.com>
2025-12-17 13:17:58 +0600

    qol

--- server_orchestrator/asgi.py
+++ server_orchestrator/asgi.py
@@ -1,9 +1,14 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
 from django.core.asgi import get_asgi_application
-import game_manager.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server_orchestrator.settings")
+
+# 🔴 THIS IS MANDATORY
+django.setup()
+
+import game_manager.routing  # noqa: E402 (import AFTER setup)
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
