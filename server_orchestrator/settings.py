251cbef184ef8369ce0f0baf9f4f7f71fdaba96d
Al-amin Khalid <alaminkhalid2@gmail.com>
2025-12-21 17:41:21 +0600

    Update settings.py

--- server_orchestrator/settings.py
+++ server_orchestrator/settings.py
@@ -86,7 +86,7 @@ CHANNEL_LAYERS = {
 
 # Redis Configuration
 REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 16379))
 REDIS_DB = int(os.environ.get('REDIS_DB', 0))
 
 # Celery Configuration
